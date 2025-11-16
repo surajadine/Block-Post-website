81bacec901bf5f9308e35a33719b03970bc72d44
mahamat <surajadine>
2025-11-17 01:32:12 +0530

    Building Class Based Views

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list,name='post_list'),
+    # path('', views.post_list,name='post_list'),
+    path('', views.PostListView.as_view(),name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/', views.post_details, name='post_detail')
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,29 @@
 from django.shortcuts import render,get_object_or_404
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
 
 # Create your views here.
-def post_list(request):
-    post_list = Post.objects.all()
-    paginator = Paginator(post_list,1)
-    page_number = request.GET.get('page',1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
+# def post_list(request):
+#     post_list = Post.objects.all()
+#     paginator = Paginator(post_list,1)
+#     page_number = request.GET.get('page',1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request,'blog/post/list.html',{'posts':posts})
 
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
     
-    return render(request,'blog/post/list.html',{'posts':posts})
+    
 
 def post_details(request,year,month,day,slug):
     post = get_object_or_404(Post,

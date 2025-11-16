8b8e2ae5930bc2544f6c266ca6c3942fb3a979f2
mahamat <surajadine>
2025-11-16 23:16:02 +0530

    Adding Pagination To The Post List View

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render,get_object_or_404
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from .models import Post
+
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.all()
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list,1)
+    page_number = request.GET.get('page',1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    
     return render(request,'blog/post/list.html',{'posts':posts})
 
 def post_details(request,year,month,day,slug):

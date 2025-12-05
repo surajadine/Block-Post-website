b60b9b29a652af4e03dafda17c8b75b56dcfe9b9
mahamat <surajadine>
2025-12-05 21:18:37 +0530

    installing postresql

--- blog/sitemap.py
+++ blog/sitemap.py
@@ -2,8 +2,10 @@ from django.contrib.sitemaps import Sitemap
 from .models import Post
 
 class ArticleSitemap(Sitemap):
+    changefreq = "weekly" # Optional: how frequently the page is likely to change
+    priority = 0.7        # Optional: the priority of this URL relative to other URLs on your site (0.0 to 1.0)
     def items(self):
         return Post.objects.all()
       
     def lastmod(self, obj):
-        return obj.lastedit_date
+        return obj.updated
